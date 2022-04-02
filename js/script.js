8c09f3559ae40d910eb31aba647fc98731049037
SPatel29 <sunnypatelc9@gmail.com>
2022-04-02 14:53:14 -0500

    implemented google maps API. JS code in js file and css style of map in styles.css

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function init() {
+    function initMap() {
+        var map;
+        map = new google.maps.Map(document.getElementById("map"), {
+            center: { lat: -34.397, lng: 150.644 },
+            zoom: 8,
+        });
+    }
+    initMap();
+
+}
+
+window.addEventListener('load', init)
