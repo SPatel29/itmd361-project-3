fbe774da32f03294fdabecb49354b157f6d4ea7c
SPatel29 <sunnypatelc9@gmail.com>
2022-04-02 15:33:16 -0500

    added marker for a long and lat

--- js/script.js
+++ js/script.js
@@ -1,10 +1,17 @@
 function init() {
     function initMap() {
         var map;
+        var myLatLng = new google.maps.LatLng(-34.397, 150.644);
         map = new google.maps.Map(document.getElementById("map"), {
             center: { lat: -34.397, lng: 150.644 },
             zoom: 8,
         });
+        new google.maps.Marker({
+            position: myLatLng,
+            map,
+            title: "Hello World!",
+        });
+
     }
     initMap();
 
